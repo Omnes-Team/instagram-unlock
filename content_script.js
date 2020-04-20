6f301d88c2b0d8e52bca076f1a06fd5da82a45fe
Italox <italoaurelior@gmail.com>
2020-04-20 14:28:43 -0300

    :rotating_light: Add Standard JS Lint

--- content_script.js
+++ content_script.js
@@ -1,34 +1,35 @@
-// Select node
-var targetNode = document.body;
+/* global MutationObserver */
+
+const targetNode = document.body
 
 // Callback functions to execute when mutations are observed
 const presentationObserver = function (mutationsList) {
   // Use traditional 'for loops' for IE 11
-  for (let mutation of mutationsList) {
-    if (mutation.addedNodes[0].attributes.role.value === "presentation") {
+  for (const mutation of mutationsList) {
+    if (mutation.addedNodes[0].attributes.role.value === 'presentation') {
       // Remove modal and form presentation from DOM
       document
         .querySelectorAll('[role="presentation"]')
         .forEach(function (element) {
-          element.remove();
-        });
+          element.remove()
+        })
     }
   }
-};
+}
 
 const bodyObserver = function (mutationsList) {
-  for (let mutation of mutationsList) {
+  for (const mutation of mutationsList) {
     // Watch <body> changes and change style if is hidden
-    if (mutation.target.style.overflow === "hidden") {
-      targetNode.style.overflow = "initial";
+    if (mutation.target.style.overflow === 'hidden') {
+      targetNode.style.overflow = 'initial'
     }
   }
-};
+}
 
 // Create an observer instance linked to the callback function
-const presentationInstance = new MutationObserver(presentationObserver);
-const bodyInstance = new MutationObserver(bodyObserver);
+const presentationInstance = new MutationObserver(presentationObserver)
+const bodyInstance = new MutationObserver(bodyObserver)
 
 // Start observing the target node for configured mutations
-presentationInstance.observe(targetNode, { childList: true });
-bodyInstance.observe(targetNode, { attributes: true });
+presentationInstance.observe(targetNode, { childList: true })
+bodyInstance.observe(targetNode, { attributes: true })
